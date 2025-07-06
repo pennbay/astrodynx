334a3e2fa57777d0f0fe230c3b5848654ade35a8
pennbay <pennbay@outlook.com>
2025-07-06 12:39:02 +0000

    feat: add imgconverter extension to Sphinx configuration

--- docs/conf.py
+++ docs/conf.py
@@ -25,6 +25,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.mathjax",
     "sphinx.ext.intersphinx",
+    "sphinx.ext.imgconverter",
     "sphinx_math_dollar",
     "sphinx_autodoc_typehints",
     "sphinx_copybutton",
